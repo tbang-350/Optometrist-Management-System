490e760b7e1abd6923a094c923d698a03d3ecaac
tbang-350 <tbway350@gmail.com>
2023-11-20 17:54:55 +0300

    updated background image to improve load speed

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Dashboard | Inventory System</title>
+    <title>Optometrist | Inventory System</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesdesign" name="author" />

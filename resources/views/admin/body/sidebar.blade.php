ef28e03094639b86ffe4f0d9ba181b86e1c801fd
tbang-350 <tbway350@gmail.com>
2023-11-23 11:15:40 +0300

    minor changes to sidebar , showing items based on roles

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -33,31 +33,34 @@
                     </li>
                 @endif
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-shield-user-fill"></i>
-                        <span>Customers</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('customer.all') }}">All Customers</a></li>
-                        <li><a href="{{ route('credit.customer') }}">Credit Customers</a></li>
+                @if ($role == 1 || $role == 2)
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-shield-user-fill"></i>
+                            <span>Customers</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('customer.all') }}">All Customers</a></li>
+                            <li><a href="{{ route('credit.customer') }}">Credit Customers</a></li>
 
-                        <li><a href="{{ route('paid.customer') }}">Paid Customers</a></li>
-                         <li><a href="{{ route('customer.wise.report') }}">Customer Wise Report</a></li>
-                    </ul>
-                </li>
+                            <li><a href="{{ route('paid.customer') }}">Paid Customers</a></li>
+                            <li><a href="{{ route('customer.wise.report') }}">Customer Wise Report</a></li>
+                        </ul>
+                    </li>
 
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-shield-user-fill"></i>
-                        <span>Services</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('service.all') }}">All Services</a></li>
-                        {{-- <li><a href="email-read.html">Read Email</a></li> --}}
-                    </ul>
-                </li>
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-shield-user-fill"></i>
+                            <span>Services</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('service.all') }}">All Services</a></li>
+                            {{-- <li><a href="email-read.html">Read Email</a></li> --}}
+                        </ul>
+                    </li>
+                @endif
+
 
                 <li class="menu-title">Consultation &amp; Prescription</li>
 
@@ -88,71 +91,70 @@
 
                 </li>
 
-                <li class="menu-title">Product &amp; Purchase </li>
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-hotel-fill"></i>
-                        <span>Manage Products</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        {{-- <li><a href="{{ route('supplier.all') }}">Suppliers</a></li> --}}
-                        {{-- <li><a href="{{ route('unit.all') }}">Units</a></li> --}}
-                        <li><a href="{{ route('category.all') }}">Categories</a></li>
-                        <li><a href="{{ route('product.all') }}">Products</a></li>
-                    </ul>
+                @if ($role == 1 || $role == 2)
+                    <li class="menu-title">Product &amp; Purchase </li>
 
-
-                </li>
-
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-shopping-cart-fill"></i>
-                        <span>Manage Purchases</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('purchase.all') }}">All Purchases</a></li>
-                        {{-- <li><a href="{{ route('purchase.pending') }}">Purchase Approval</a></li> --}}
-                    </ul>
-
-                </li>
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-hotel-fill"></i>
+                            <span>Manage Products</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            {{-- <li><a href="{{ route('supplier.all') }}">Suppliers</a></li> --}}
+                            {{-- <li><a href="{{ route('unit.all') }}">Units</a></li> --}}
+                            <li><a href="{{ route('category.all') }}">Categories</a></li>
+                            <li><a href="{{ route('product.all') }}">Products</a></li>
+                        </ul>
 
 
-                <li class="menu-title">Invoice</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class=" ri-edit-box-fill"></i>
-                        <span>Product Invoice</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('invoice.all') }}">All Invoices</a></li>
-                        <li><a href="{{ route('invoice.pending.list') }}">Invoice Approval</a></li>
-                    </ul>
-
-                </li>
+                    </li>
 
 
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-shopping-cart-fill"></i>
+                            <span>Manage Purchases</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('purchase.all') }}">All Purchases</a></li>
+                            {{-- <li><a href="{{ route('purchase.pending') }}">Purchase Approval</a></li> --}}
+                        </ul>
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-shield-user-fill"></i>
-                        <span>Service Invoice</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('service.invoice.all') }}">All Service Invoices</a></li>
-                        {{-- <li><a href="email-read.html">Read Email</a></li> --}}
-                    </ul>
-                </li>
+                    </li>
+
+
+                    <li class="menu-title">Invoice</li>
+
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class=" ri-edit-box-fill"></i>
+                            <span>Product Invoice</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('invoice.all') }}">All Invoices</a></li>
+                            <li><a href="{{ route('invoice.pending.list') }}">Invoice Approval</a></li>
+                        </ul>
+
+                    </li>
 
 
 
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-shield-user-fill"></i>
+                            <span>Service Invoice</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('service.invoice.all') }}">All Service Invoices</a></li>
+                            {{-- <li><a href="email-read.html">Read Email</a></li> --}}
+                        </ul>
+                    </li>
 
 
-                <li class="menu-title">Reports</li>
+                    <li class="menu-title">Reports</li>
 
-                {{-- <li>
+                    {{-- <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="ri-account-circle-line"></i>
                         <span>Customer</span>
@@ -163,23 +165,26 @@
                     </ul>
                 </li> --}}
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-profile-line"></i>
-                        <span>All Reports</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{ route('daily.service.invoice.report') }}">Daily Service Invoice Report</a></li>
-                        <li><a href="{{ route('service.invoice.payment.report') }}">Service Payment Option Report</a></li>
-                        <li><a href="{{ route('stock.report') }}">Stock Report</a></li>
-                        <li><a href="{{ route('daily.invoice.report') }}">Daily Invoice Report</a></li>
-                        <li><a href="{{ route('daily.purchase.report') }}">Daily Purchase Report</a></li>
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow waves-effect">
+                            <i class="ri-profile-line"></i>
+                            <span>All Reports</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('daily.service.invoice.report') }}">Daily Service Invoice Report</a>
+                            </li>
+                            <li><a href="{{ route('service.invoice.payment.report') }}">Service Payment Option
+                                    Report</a>
+                            </li>
+                            <li><a href="{{ route('stock.report') }}">Stock Report</a></li>
+                            <li><a href="{{ route('daily.invoice.report') }}">Daily Invoice Report</a></li>
+                            <li><a href="{{ route('daily.purchase.report') }}">Daily Purchase Report</a></li>
 
-                    </ul>
-                </li>
+                        </ul>
+                    </li>
+                @endif
 
 
-                
 
                 {{-- <li class="mm-active">
                     <a href="javascript: void(0);" class="has-arrow waves-effect" aria-expanded="true">
